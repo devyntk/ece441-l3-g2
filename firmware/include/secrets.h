b2ef114d2e34886ab53237992b8ae45d8079575e
devyntk <bkeeneykid@me.com>
2022-10-12 21:40:01 -0500

    add lmic-node starter code

--- firmware/include/secrets.h
+++ firmware/include/secrets.h
@@ -1,11 +1,67 @@
+/*******************************************************************************
+ *
+ *  File:          lorawan-keys_example.h
+ * 
+ *  Function:      Example for lorawan-keys.h required by LMIC-node.
+ *
+ *  Copyright:     Copyright (c) 2021 Leonel Lopes Parente
+ *
+ *  Important      ██ DO NOT EDIT THIS EXAMPLE FILE (see instructions below) ██
+ * 
+ *  Decription:    lorawan-keys.h defines LoRaWAN keys needed by the LMIC library.
+ *                 It can contain keys for both OTAA and for ABP activation.
+ *                 Only the keys for the used activation type need to be specified.
+ * 
+ *                 It is essential that each key is specified in the correct format.
+ *                 lsb: least-significant-byte first, msb: most-significant-byte first.
+ * 
+ *                 For security reasons all files in the keyfiles folder (except file
+ *                 lorawan-keys_example.h) are excluded from the Git(Hub) repository.
+ *                 Also excluded are all files matching the pattern *lorawan-keys.h.
+ *                 This way they cannot be accidentally committed to a public repository.
+ * 
+ *  Instructions:  1. Copy this file lorawan-keys_example.h to file lorawan-keys.h
+ *                    in the same folder (keyfiles).
+ *                 2. Place/edit required LoRaWAN keys in the new lorawan-keys.h file.
+ *
+ ******************************************************************************/
 
-#include <Arduino.h>
+#pragma once
 
-// Network Session Key (MSB)
-uint8_t NwkSkey[16] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+#ifndef LORAWAN_KEYS_H_
+#define LORAWAN_KEYS_H_
 
-// Application Session Key (MSB)
-uint8_t AppSkey[16] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+// Optional: If DEVICEID is defined it will be used instead of the default defined in the BSF.
+// #define DEVICEID "<deviceid>"
 
-// Device Address (MSB)
-uint8_t DevAddr[4] = {0x00, 0x00, 0x00, 0x00};
+// Keys required for OTAA activation:
+
+// End-device Identifier (u1_t[8]) in lsb format
+#define OTAA_DEVEUI 0xEB, 0x65, 0x05, 0xD0, 0x7E, 0xD5, 0xB3, 0x70
+
+// Application Identifier (u1_t[8]) in lsb format
+#define OTAA_APPEUI 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+
+// Application Key (u1_t[16]) in msb format
+#define OTAA_APPKEY 0x6D, 0x0B, 0xC2, 0xFA, 0x1C, 0x93, 0xEB, 0x7D, 0xB9, 0x95, 0xA0, 0xCE, 0x7D, 0x34, 0x89, 0x1E
+
+
+// -----------------------------------------------------------------------------
+
+// Optional: If ABP_DEVICEID is defined it will be used for ABP instead of the default defined in the BSF.
+// #define ABP_DEVICEID "<deviceid>"
+
+// Keys required for ABP activation:
+
+// End-device Address (u4_t) in uint32_t format. 
+// Note: The value must start with 0x (current version of TTN Console does not provide this).
+#define ABP_DEVADDR 0x00000000
+
+// Network Session Key (u1_t[16]) in msb format
+#define ABP_NWKSKEY 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+
+// Application Session K (u1_t[16]) in msb format
+#define ABP_APPSKEY 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+
+
+#endif  // LORAWAN_KEYS_H_
